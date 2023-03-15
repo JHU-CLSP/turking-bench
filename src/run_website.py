78b8ae4251ba469dd93c9e4ef2af3198b5bec976
Yeganeh Kordi <yegane.kordi@gmail.com>
2023-03-16 02:18:13 +0330

    update

--- src/run_website.py
+++ src/run_website.py
@@ -3,10 +3,10 @@ import os
 
 git_url = "https://github.com/hltcoe/turkle.git" 
 repo_dir = "Turkle" 
-#Repo.clone_from(git_url, repo_dir) 
+Repo.clone_from(git_url, repo_dir) 
 
 os.chdir(repo_dir) 
-#os.system("pip install -r requirements.txt")
-#os.system("python manage.py migrate")
-#os.system("python manage.py createsuperuser")
+os.system("pip install -r requirements.txt")
+os.system("python manage.py migrate")
+os.system("python manage.py createsuperuser")
 os.system("python manage.py runserver 0.0.0.0:8000")
