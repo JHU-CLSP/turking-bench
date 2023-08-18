16d680ff8218ee8d780291fdeb044e121e5c0f65
Daniel Khashabi <danielk@jhu.edu>
2023-08-18 19:17:05 -0400

    - fixed the issue with uploading the following tasks: ATOMIC - NL Rephrase 16, umber of sense

--- src/3.upload_tasks.py
+++ src/3.upload_tasks.py
@@ -5,6 +5,10 @@ from Turkle.scripts.client import TurkleClient
 import sys
 import os
 import pandas as pd
+from colorama import init as colorama_init
+from colorama import Fore, Back, Style
+
+colorama_init(autoreset=True)
 
 parser = argparse.ArgumentParser(
     description="Upload a batch of tasks to Turkle",
@@ -16,42 +20,40 @@ parser.add_argument("-p", help="admin password")
 parser.add_argument("--server", help="protocol://hostname:port", default="http://localhost:8000")
 args = parser.parse_args()
 
+
 class Options:
     def __init__(self, batch_name, project_name, template, csv):
         self.login = 0
         self.num = 1
-        self.batch_name = batch_name    
+        self.batch_name = batch_name
         self.project_name = project_name
         self.template = template
         self.csv = csv
 
 
 client = TurkleClient(args.server, args.u, args.p)
-for root, dirs, files in os.walk('../tasks'):
+limited_directoris = ["ATOMIC - NL Rephrase 16", "Number of sense"]
+for root, dirs, _ in os.walk('../tasks'):
     for dir in dirs:
-        try:
-            temp = ''
-            csvpath = ''
-            dir_path = os.path.join(root, dir)
-            for file in os.listdir(dir_path):
-                if file.endswith('.html'):
-                    temp = os.path.join(dir_path, file)
-                if file.endswith('input.csv'):
-                    csvpath = os.path.join(dir_path, file)
-                    input_encoding = 'utf8'
-                    output_encoding = 'utf8'
-                    df = pd.read_csv(csvpath, encoding=input_encoding)
-                    df.to_csv(csvpath, index=False, encoding=output_encoding)
-            print(dir)
-            options = Options(batch_name=dir, project_name=dir,
-                              template=temp,
-                              csv=csvpath)
-            result = client.upload(options)
-            if result:
-                print("Success")
-        except Exception as e:
-            print(f"An error occurred: {e}")
-            # unsuccessful_tasks.append(dir)
+        if dir not in limited_directoris:
+            continue
+        temp = ''
+        csvpath = ''
+        dir_path = os.path.join(root, dir)
+        for file in os.listdir(dir_path):
+            if file.endswith('.html'):
+                temp = os.path.join(dir_path, file)
+            if file.endswith('input.csv'):
+                csvpath = os.path.join(dir_path, file)
+                input_encoding = 'utf8'
+                output_encoding = 'utf8'
+                df = pd.read_csv(csvpath, encoding=input_encoding)
+                df.to_csv(csvpath, index=False, encoding=output_encoding)
+        print(dir)
+        options = Options(batch_name=dir, project_name=dir,
+                          template=temp,
+                          csv=csvpath)
+        result = client.upload(options)
+        if result:
+            print(f"{Fore.GREEN}Success")
 
-# TODO: undefined variable to be fixed.
-# print(f"Unsuccessful tasks: {unsuccessful_tasks}")
