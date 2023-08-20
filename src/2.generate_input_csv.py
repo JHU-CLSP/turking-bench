7a322924898e5aa3c66862674d9c8a88b23f5a26
Daniel Khashabi <danielk@jhu.edu>
2023-08-20 16:54:32 -0400

    - simplfiy file upload.

--- src/2.generate_input_csv.py
+++ src/2.generate_input_csv.py
@@ -1,32 +1,41 @@
 import pandas as pd
 import os
+
 """
 Script for removing unnecessary data from csv files
 """
+
 
 def create_input(csv_file):
     df = pd.read_csv(csv_file, low_memory=False)
     df = df.loc[:, ~df.columns.str.startswith('Answer.')]
     df.drop_duplicates(inplace=True)
-    df.to_csv(csv_file.replace('batch.csv', 'input.csv') , encoding='utf-8-sig', index=False)
+    df.to_csv(csv_file.replace('batch.csv', 'input.csv'), encoding='utf-8-sig', index=False)
+
+ROOT = '../tasks'
 
 if __name__ == '__main__':
     # ensure that ../tasks is available
-    if not os.path.exists('../tasks'):
+
+    if not os.path.exists(ROOT):
         raise Exception("No directory named `tasks` found. Make sure that you run this script in the `src/` directory")
 
-    for root, dirs, _ in os.walk('../tasks'):
-        # if files is empty then show an error
-        if not dirs or len(dirs) == 0:
-            raise Exception(f"No files in the specified directory `{root}`: {dirs}")
+    items = os.listdir(ROOT)
+    # if files is empty then show an error
+    if len(items) == 0:
+        raise Exception(f"No files in the specified directory `{items}`: {ROOT}")
 
-        for dir in dirs:
-            file = os.path.join(root, dir, 'batch.csv')
+    for item in items:
 
-            # make sure the file exists
-            if not os.path.exists(file):
-                raise Exception(f"File `{file}` does not exist")
+        # skip if not a dir
+        if not os.path.isdir(os.path.join(ROOT, item)):
+            continue
 
-            path = os.path.join(root, file)
-            print(' ** Reading: ' + path)
-            create_input(path)
+        file = os.path.join(ROOT, item, 'batch.csv')
+
+        # make sure the file exists
+        if not os.path.exists(file):
+            raise Exception(f"File `{file}` does not exist")
+
+        print(' ** Reading: ' + file)
+        create_input(path)
