6a90730521b27de17a53a1082e04533cbe30d4b6
klxu03 <beaver2025@gmail.com>
2023-09-27 12:59:26 -0400

    Big linux push, not fully tested comprehensive_tests

--- src/comprehensive_test.py
+++ src/comprehensive_test.py
@@ -2,41 +2,31 @@ eval = __import__('4_run_evaluation')
 from evaluation.actions import MyActions
 from evaluation.baselines import Baseline
 from utils.hidden_prints import HiddenPrints
-import statistics
 
 evaluation = eval.Evaluation(solver_type="oracle", tasks="all",
                              do_eval=True, dump_features=True, report_field_stats=True)
 
 
-def test_actions():
-    baseline = Baseline(driver=evaluation.driver, actions=evaluation.actions)
-    action_list = baseline.get_action_list()
-    print(action_list)
-    assert len(action_list) > 0, f"The action list should not be empty: {action_list}"
-
-    encoded_actions_prompt = baseline.get_encoded_action_list()
-    print(encoded_actions_prompt)
-    assert len(encoded_actions_prompt) > 0, f"The encoded actions prompt should not be empty: {encoded_actions_prompt}"
-
-
 def test_evaluation():
-    evaluation.enumerate_tasks(max_instance_count=1)
+    # dictionary mapping {task_name, {num_successes, success_percentage, num_errors, avg_failing_score}}
+    results = evaluation.enumerate_comprehensive_tasks(max_instance_count=1000) # dictionary of results
 
+    # Global statistics
+    tasks_succeeded = 0
+    tasks_with_errors = 0
+    tasks_failed = 0 # not counting tasks_with_errors
+
+    for task in results:
+        result = results[task]
+        if result["success_percentage"] > 0.99:
+            tasks_succeeded += 1 
+        elif result["num_errors"] > 0:
+            tasks_with_errors += 1
+        else:
+            tasks_failed += 1
+
+        print(f"Task: {task} | Successes: {result['num_successes']} | Success Percentage: {result['success_percentage']} | Errors: {result['num_errors']} | Avg Failing Score: {result['avg_failing_score']}")
 
 if __name__ == "__main__":
-    print("Running initial pass on tests without logs")
-    try:
-        with HiddenPrints():
-            test_evaluation()
-            test_actions()
-    except Exception as error:
-        print("An error occurred:", error)
-        print("Rerunning tests with logs now")
-        test_evaluation()
-        test_actions()
-
-    # TODO: test that we can apply the gold labels on the tasks
-
-    # TODO: test the actions
-
-    # TODO: test the evaluation
+    print("Running comprehensive tests")
+    test_evaluation()
