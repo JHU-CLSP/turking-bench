c2c7a907c6ac9105c9c3f60546171be240631e1a
klxu03 <beaver2025@gmail.com>
2023-10-04 19:55:17 -0400

    Half fixing ATOMIC Seq 9, Deleted Answer.SequenceId col and now full score. however no actual answering happening

--- src/run_single.py
+++ src/run_single.py
@@ -11,8 +11,8 @@ from utils.hidden_prints import HiddenPrints
 import logging
 
 TURKLE_URL = "http://localhost:8000"
-TEST_NAME = "Dialogue safety (socialchemistry) 5"
-SPECIFIED_INDEX = 0
+TEST_NAME = "ATOMIC - Required Objects (Sequence) 9"
+SPECIFIED_INDEX = 3
 RUN_ALL = False
 
 class Run(run_eval.Evaluation):
