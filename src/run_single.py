38782dc81ced3b19476a200b68288f91f044864e
klxu03 <beaver2025@gmail.com>
2023-10-04 19:45:59 -0400

    Fixed Dialogue safety 5

--- src/run_single.py
+++ src/run_single.py
@@ -11,8 +11,8 @@ from utils.hidden_prints import HiddenPrints
 import logging
 
 TURKLE_URL = "http://localhost:8000"
-TEST_NAME = "Photo Collection GVDB"
-SPECIFIED_INDEX = 7
+TEST_NAME = "Dialogue safety (socialchemistry) 5"
+SPECIFIED_INDEX = 0
 RUN_ALL = False
 
 class Run(run_eval.Evaluation):
