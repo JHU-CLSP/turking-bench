6b5314bac8b883a9a7105b8176f5af66ec516af3
klxu03 <beaver2025@gmail.com>
2023-11-11 13:04:03 -0500

    create with Path

--- rel_write.py
+++ rel_write.py
@@ -1,8 +1,11 @@
 import os
+from pathlib import Path
+
 
 scratch4 = "/scratch4/danielk/kxu39/turk_data"
 task_name = "task1"
-os.mkdir(f"{scratch4}/{task_name}", exist_ok=True)
+
+Path(f"{scratch4}/{task_name}").mkdir(parents=True, exist_ok=True)
 
 with open(f'{scratch4}/{task_name}/{task_name}.txt', 'w') as fp:
     fp.write("hello world")
