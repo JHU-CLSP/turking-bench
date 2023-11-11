7b5b62aba5bd3300a679767f09335048c95d3aa4
klxu03 <beaver2025@gmail.com>
2023-11-11 01:27:06 -0500

    proper script with sleeping to wait for time to run website and concurrently dump features

--- rockfish/dump_features.sh
+++ rockfish/dump_features.sh
@@ -41,4 +41,4 @@ python="~/miniconda3/envs/turk/bin/python"
 bash="/bin/bash"
 
 cd turk-instructions/src
-$bash 1_run_website.sh && $python dump_features.py dmp$SLURM_ARRAY_TASK_ID $SLURM_TASK_MAX
+$bash 1_run_website.sh & sleep 10 && $python dump_features.py dmp$SLURM_ARRAY_TASK_ID $SLURM_TASK_MAX
