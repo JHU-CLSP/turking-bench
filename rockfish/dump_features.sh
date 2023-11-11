475e6d55d1d73bd68a02ec6c24c38e18e16a4599
klxu03 <beaver2025@gmail.com>
2023-11-11 00:54:13 -0500

    start of dump_featuers rockfish script, need to add job array

---
+++ rockfish/dump_features.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+#SBATCH --job-name=dump
+#SBATCH --output=dump.out.log
+#SBATCH --error=dump.err.log
+#
+# Number of tasks needed for this job. Generally, used with MPI jobs
+#SBATCH --ntasks=1
+#SBATCH --partition=defq
+#
+# Time format = HH:MM:SS, DD-HH:MM:SS
+#SBATCH --time=72:00:00
+#
+# Minimum memory required per allocated  CPU  in  MegaBytes.
+#SBATCH --mem-per-cpu=48000
+#SBATCH -A danielk
+#
+# Send mail to the email address when the job fails
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kxu39@jhu.edu
+
+# Load necessary modules
+module load anaconda
+
+source ~/.bashrc
+
+conda activate turk
+
+module list
+
+python --version
+which python
+
+source .env
+
+# Run the Python script
+python="~/miniconda3/envs/turk/bin/python"
+bash="/bin/bash"
+
+cd turk-instructions/src
+$bash 1_run_website.sh &
+$python dump_features.py dmp1
