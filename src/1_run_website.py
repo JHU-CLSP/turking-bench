97562084c80c7d688ea759ae863e5d6552093bea
klxu03 <beaver2025@gmail.com>
2023-11-11 11:14:55 -0500

    dirty fix of run_web_server

--- src/1_run_website.py
+++ src/1_run_website.py
@@ -1,19 +1,9 @@
 import subprocess
-import sys
 import os
 
-# what conda env am I in (e.g., where is my Python process from)?
-ENVBIN = sys.exec_prefix
-
-# what binaries am I looking for that are installed in this env?
-PYTHON = os.path.join(ENVBIN, "bin", "python")
-
-# let's make sure they exist, no typos.
-for bin in (PYTHON):
-    assert os.path.exists(bin), "missing binary {} in env {}".format(bin, ENVBIN)
-
 def run_web_server():
-    subprocess.Popen([PYTHON, "Turkle/manage.py", "runserver", "0.0.0.0:8000"])
+    subprocess.run('bash -c "source activate root; python -V"', shell=True)
+    subprocess.Popen(["python", "Turkle/manage.py", "runserver", "0.0.0.0:8000"])
 
 if __name__ == "__main__":
     assert os.path.exists("Turkle/manage.py"), f"Turkle must be installed in the Turkle directory before running this script to start the web server"
