9e50c44e991ae891782f1650591e7d3fd4ba4134
klxu03 <beaver2025@gmail.com>
2023-11-11 13:02:21 -0500

    fix mkdir

--- rel_write.py
+++ rel_write.py
@@ -1,9 +1,8 @@
 import os
-import json
 
 scratch4 = "/scratch4/danielk/kxu39/turk_data"
 task_name = "task1"
-os.makedirs(f"{scratch4}/{task_name}", exist_ok=True)
+os.mkdir(f"{scratch4}/{task_name}", exist_ok=True)
 
-with open(f'{scratch4}/{task_name}/{task_name}.json', 'w') as fp:
+with open(f'{scratch4}/{task_name}/{task_name}.txt', 'w') as fp:
     fp.write("hello world")
