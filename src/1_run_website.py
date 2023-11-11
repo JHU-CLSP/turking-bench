c7e686cc59c91ee2f24889bc33ad4b096dad0199
klxu03 <beaver2025@gmail.com>
2023-11-11 11:20:51 -0500

    pipe output

--- src/1_run_website.py
+++ src/1_run_website.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 def run_web_server():
-    subprocess.Popen("conda run -n turk python Turkle/manage.py runserver 0.0.0.0:8000".split())
+    subprocess.Popen("conda run -n turk python Turkle/manage.py runserver 0.0.0.0:8000".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 if __name__ == "__main__":
     assert os.path.exists("Turkle/manage.py"), f"Turkle must be installed in the Turkle directory before running this script to start the web server"
