f150a270fcfa5186b66e2ce0dfcb93649e1fee8e
klxu03 <beaver2025@gmail.com>
2023-11-11 12:59:23 -0500

    create the dir if nonexistent

--- rel_write.py
+++ rel_write.py
@@ -3,6 +3,7 @@ import json
 
 scratch4 = "/scratch4/danielk/kxu39/turk_data"
 task_name = "task1"
+os.makedirs(f"{scratch4}/{task_name}", exist_ok=True)
 
 with open(f'{scratch4}/{task_name}/{task_name}.json', 'w') as fp:
     fp.write("hello world")
