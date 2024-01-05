5a27f6841c3cacb29efd839fff9a04fa9f22cc83
klxu03 <beaver2025@gmail.com>
2024-01-05 23:46:32 +0100

    fixed annotation subj_obj with data cleaning

--- src/utils/clean_csv.py
+++ src/utils/clean_csv.py
@@ -94,10 +94,10 @@ def clean_empty(csv_file):
     df = pd.read_csv(csv_file, low_memory=False)
     for i, row in df.iterrows():
         for col in df.columns:
-            if col.startswith("Answer."):
+            if not (col.startswith("Answer.") and col.endswith("subject")):
                 continue
             if pd.isnull(row[col]) or row[col] == "":
-                df.loc[i, col] = "Empty"
+                df.loc[i, col] = "--NO SUBJECT--"
     df.to_csv(csv_file , encoding='utf-8-sig', index=False)
 
 # Clean an "Empty" cell with certain properties (in certain col) into an actually empty cell, reversing clean_empty 
@@ -107,7 +107,7 @@ def clean_unempty(csv_file):
         for col in df.columns:
             if not col.startswith("Answer."):
                 continue
-            if row[col] == "Empty":
+            if row[col] == "--NO SUBJECT--":
                 df.loc[i, col] = ""
     df.to_csv(csv_file , encoding='utf-8-sig', index=False)
 
@@ -118,4 +118,4 @@ if __name__ == '__main__':
             if file.endswith('.csv') and file.startswith('batch'):
                 if root.split("/")[1] in files_to_edit:
                     print('Cleaning ' + os.path.join(root, file))
-                    clean_unempty(os.path.join(root, file))
+                    clean_empty(os.path.join(root, file))
