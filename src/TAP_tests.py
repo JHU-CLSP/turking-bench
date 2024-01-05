02a5d63251b06cf474d8b1527ed4628349ab84a7
klxu03 <beaver2025@gmail.com>
2024-01-05 23:23:23 +0100

    tap tests max instance count 2

--- src/TAP_tests.py
+++ src/TAP_tests.py
@@ -14,7 +14,8 @@ def test_evaluation():
         results = evaluation.enumerate_tap_tasks_random(max_instance_count=2) # dictionary of results
     else:
         # dictionary mapping {task_name, {num_successes, num_errors, num_failing, sum_failing_scores} }
-        results = evaluation.enumerate_tap_tasks(max_instance_count=1000) # dictionary of results
+        max_instance_count = 2
+        results = evaluation.enumerate_tap_tasks(max_instance_count=max_instance_count) # dictionary of results
 
     # Global statistics
     tasks_succeeded = 0
