3860935ac92e783ce1159c50a823cc7884f0bd23
klxu03 <beaver2025@gmail.com>
2024-01-08 22:05:44 -0500

    reordered cols, realizing dialogue safety socialchem 5 task needs lots of manual fixing of rows

--- src/run_single.py
+++ src/run_single.py
@@ -11,8 +11,8 @@ from utils.hidden_prints import HiddenPrints
 import logging
 
 TURKLE_URL = "http://localhost:8000"
-TEST_NAME = "ATOMIC - Required Objects (Sequence) 9"
-SPECIFIED_INDEX = 77
+TEST_NAME = "Dialogue safety (socialchemistry) 5"
+SPECIFIED_INDEX = 26
 RUN_ALL = False
 
 class Run(evaluation_class.Evaluation):
