06fe597dc5d9e258bbae87e4d04750d7bcbe747b
klxu03 <beaver2025@gmail.com>
2024-02-13 14:29:10 -0500

    test_screenshot

---
+++ src/test_screenshot.py
@@ -0,0 +1,19 @@
+from pyvirtualdisplay import Display
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from io import BytesIO
+from PIL import Image
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+options = Options()
+
+browser = webdriver.Chrome(options=options)
+browser.get('http://www.google.com')
+browser.save_screenshot('screenie.png')
+# screenshot = browser.get_screenshot_as_png()
+# image = Image.open(BytesIO(screenshot))
+browser.quit()
+
+display.stop()
