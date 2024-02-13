80d4a1d57c235feb0e732852be0a1f59a0b202d4
klxu03 <beaver2025@gmail.com>
2024-02-13 15:39:15 -0500

    general exception

--- src/evaluation/vision.py
+++ src/evaluation/vision.py
@@ -10,8 +10,8 @@ from PIL import Image, ImageDraw, ImageFont, ImageGrab
 try:
     import pyautogui
     pyautogui_installed = True
-except ImportError:
-    print("Error importing pyautogui")
+except Exception as e:
+    print(f"Error importing pyautogui {e}")
     pyautogui_installed = False
 from openai import OpenAI
 from typing import List, Tuple
