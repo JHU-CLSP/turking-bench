573da593839adcbe75231002e64f4484bde9d35b
klxu03 <beaver2025@gmail.com>
2024-02-15 14:35:41 -0500

    claude text model in

--- src/evaluation/text.py
+++ src/evaluation/text.py
@@ -80,6 +80,7 @@ class GPT4Model(TextModel):
                 time.sleep(30)
                 print(f"Error getting action from GPT4V model {e}, trying again, current fail_count is {fail_count}")
 
+        print(f"OpenAI Response: {response.choices[0].message.content}")
         return response.choices[0].message.content
 
 class OLlamaTextModel(TextModel):
@@ -98,55 +99,42 @@ class ClaudeModel(TextModel):
         self.client = AnthropicBedrock(aws_access_key=aws_access_key, aws_secret_key=aws_secret_key, aws_region="us-east-1")
 
     def get_text_baseline_action(self, input_name: str, html_code: str, num_demonstrations: int, use_relevant_html: bool) -> str:
-        messages = [{
-            "role": "assistant",
-            "content": text_oracle_instructions()
-        }]
+        system_instruction = f"""<instructions>
+        {text_oracle_instructions()}
+        </instructions>\n
+        """
+
+        prompt = system_instruction
 
         for idx, instance in enumerate(islice(few_shot_examples(), num_demonstrations)):
-            user_message = {
-                "role": "user",
-                "content": instance[3] if use_relevant_html else instance[0]
-            }
-
-            assistant_message = {
-                "role": "assistant",
-                "content": instance[2]
-            }
-
-            messages.append(user_message)
-            messages.append(assistant_message)
+            prompt += f"{HUMAN_PROMPT}{instance[3] if use_relevant_html else instance[0]}{AI_PROMPT}{instance[2]}"
 
 
-        new_message = {
-            "role": "user",
-            "content": f"""
+        new_message = f"""
                         Input name: {input_name}
                         HTML:
                         {html_code}
                         """
-        }
-
-        messages.append(new_message)
+        prompt += f"{HUMAN_PROMPT}{new_message}{AI_PROMPT}"
 
         fail_count = 0
         response = None 
         while fail_count < 20:
             try: 
-                response = self.client.chat.completions.create(
-                    model="gpt-4-turbo-preview",
-                    messages=messages,
-                    temperature=1,
-                    max_tokens=256,
-                    top_p=1,
-                    frequency_penalty=0,
-                    presence_penalty=0
+                completion = self.client.completions.create(
+                    model="anthropic.claude-v2:1",
+                    prompt=prompt,
+                    stop_sequences=[HUMAN_PROMPT],
+                    max_tokens_to_sample=100000,
+                    temperature=0.5,
+                    top_k=250,
+                    top_p=0.5,
                 )
-
                 break
             except Exception as e:
                 fail_count += 1
                 time.sleep(30)
                 print(f"Error getting action from GPT4V model {e}, trying again, current fail_count is {fail_count}")
 
-        return response.choices[0].message.content
+        print(f"Claude response: {completion.completion}")
+        return completion.completion
